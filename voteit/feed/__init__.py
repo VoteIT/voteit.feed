640c6155ab5555ad953a8fe538c99aad99a68013
Fredrik Nilson <sassur@gmail.com>
2012-04-24 14:10:28 +0200

    adding xml renderer registration

--- voteit/feed/__init__.py
+++ voteit/feed/__init__.py
@@ -6,6 +6,8 @@ FeedMF = TranslationStringFactory('voteit.feed')
 def includeme(config):
     """ Include feed adapter, register views and content factories."""
     config.scan('voteit.feed')
+    from pyramid.chameleon_zpt import renderer_factory as zpt_rf
+    config.add_renderer('.xml', zpt_rf)
     config.add_translation_dirs('voteit.feed:locale/')
     from voteit.core.models.interfaces import IMeeting
     from voteit.feed.interfaces import IFeedHandler
